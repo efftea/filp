9b9d60e773cd106cc83017809404ef822056e76e
efftea <daanikwork@mail.ru>
2024-05-17 10:40:25 +0300

    Изменено 4.8: Добавлен комментарий.

--- kotlin_func/src/main/kotlin/projecteuler.kt
+++ kotlin_func/src/main/kotlin/projecteuler.kt
@@ -3,9 +3,10 @@ import java.util.*
 
 class projecteuler{
 
+    // Сумма цифр в степени 5 для 1 числа.
     fun sumPow(x: Int): Int = if (x<10) { Math.pow(x.toDouble(), 5.toDouble()).toInt() } else {Math.pow((x%10).toDouble(), 5.toDouble()).toInt() + sumPow(x/10)}
 
-
+    // Подсчёт суммы всех подходящих чисел.
     tailrec fun powTail(x: Int): Int =  if (x > 5 * Math.pow(9.toDouble(),5.toDouble()).toInt()) {0} else { if (x == sumPow(x)) { x + powTail(x+1)} else {powTail(x+1)} }
 
 
